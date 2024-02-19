6fa0b64d044c499f2a7f881d351dd22fc0999b53
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-02-19 18:19:38 +0100

    [Feat] Implemented the user registration, login and logout routes

---
+++ backend/src/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const { PORT, CLIENT_URL } = require('./constants');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const cors = require('cors');
+const app = express();
+
+
+// import passport middleware
+require('./middlewares/passport-middleware');
+
+// impoort routes
+const authRoutes = require('./routes/auth');
+
+//initialize middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: CLIENT_URL,
+    credentials: true
+}))
+app.use(passport.initialize());
+
+// initialize routes
+app.use('/api', authRoutes);
+
+const appStart = () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server is running at http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+    }
+}
+
+appStart();
---
+++ backend/src/middlewares/passport-middleware.js
@@ -0,0 +1,35 @@
+const passport = require('passport');
+const { Strategy } = require('passport-jwt');
+const { SECRET } = require('../constants');
+const db = require('../db');
+
+
+const cookieExtractor = (req) => {
+    let token = null;
+    if (req && req.cookies) {
+        token = req.cookies['token'];
+    }
+    return token;
+}
+
+const opts = {
+    jwtFromRequest: cookieExtractor,
+    secretOrKey: SECRET
+}
+
+passport.use(
+    new Strategy(opts, async ({ id }, done) => {
+        try {
+            const { rows } = await db.query('SELECT * FROM users WHERE id = $1', [id]);
+            
+            if (!rows.length) {
+                throw new Error('401 not authorized');
+            }
+            const user = { id: rows[0].id, username: rows[0].username, email: rows[0].email };
+            return await done(null, user);
+        } catch (error) {
+            console.log(error.message);
+            done(null, false);
+        }
+    })
+)
