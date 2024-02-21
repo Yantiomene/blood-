e538d8256ce79e98956be5e2dff5ece3ecc5c4ca
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-02-21 11:01:39 +0100

    [Update] Updated the bloodtype field

--- backend/migrations/20240221081335_create_donation_requests_table.js
+++ backend/migrations/20240221081335_create_donation_requests_table.js
@@ -6,7 +6,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('donation_requests', function (table) {
         table.increments('id').primary();
         table.integer('userId').unsigned().references('id').inTable('users').onDelete('CASCADE');
-        table.enu('bloodType', ['A', 'B', 'AB', 'O']).notNullable();
+        table.enu('bloodType',  ['A-', 'A+', 'B-', 'B+', 'AB-', 'AB+', 'O-', 'O+']).notNullable();
         table.integer('quantity').notNullable();
         table.specificType('location', 'POINT').notNullable();
         table.boolean('isFulfilled').defaultTo(false);
