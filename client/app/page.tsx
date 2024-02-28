1b42eedec1f60d526311280a13d12a373e6f2b56
esmond-adjei <xmongram@gmail.com>
2024-02-28 14:31:31 +0000

    [update] Add logo image and update header component

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,26 +1,17 @@
-import Head from 'next/head';
-import styles from '../styles/globals.css';
-import bloodplus from '../Blood+Logo.png';
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>BLOOD+ | Saves Lives</title>
-        <link rel="shortcut icon" type="image/png" href="/favicon.png" />
-      </Head>
-      <nav>
-        <div className={styles.logo}>
-          <img src="/logo.png" alt="Blood+ Logo" />
-        </div>
-        <a href="/"><button className={styles.menuButton}>HOME</button></a>
-        <a href="/aboutblood+"><button className={styles.menuButton}>ABOUT BLOOD+</button></a>
-        <a href="/contact"><button className={styles.menuButton}>CONTACT</button></a>
-        <a href="/bloodbanks"><button className={styles.menuButton}>BLOOD BANKS</button></a>
-      </nav>
-      <h1>Welcome To Blood+, a digital resource for blood access and donations</h1>
-      <section>
-        <img src="/images/blood_donor.jpg" alt="Blood Donation" className={styles.centerImage} />
+      <h1 className="text-4xl font-bold">Welcome To Blood+, a digital resource for blood access and donations</h1>
+      <section >
+        <Image
+          src="/blood_donor.jpg"
+          width={800}
+          height={400}
+          alt="Blood Donation"
+          className="mx-auto mt-10 w-[80%] rounded-lg"
+        />
       </section>
     </div>
   );
