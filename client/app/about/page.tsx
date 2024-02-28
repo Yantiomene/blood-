eee26472ef44e3b3d336c4acf0b9836445935a39
esmond-adjei <xmongram@gmail.com>
2024-02-28 14:44:59 +0000

    [cleanup and update] Delete dump.rdb file. Added blog and about pages

---
+++ client/app/about/page.tsx
@@ -0,0 +1,11 @@
+export default function AboutPage() {
+    return (
+        <div className="container mx-auto">
+            <h1 className="text-2xl font-bold mb-4">About Page</h1>
+            <p className="text-lg">
+                This is the about Page
+                {/* will put information about app, developers, etc... */}
+            </p>
+        </div>
+    );
+};
