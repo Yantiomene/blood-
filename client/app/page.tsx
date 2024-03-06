7508333a8a795a2fa7fb7e84b03d9ab42b80e080
GHMatrix <gsmensah@yahoo.com>
2024-03-06 15:36:34 +0000

    updated Landing page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -13,6 +13,10 @@ export default function Home() {
           className="mx-auto mt-10 w-[80%] rounded-lg"
         />
       </section>
+      <footer className="text-center mt-8 text-gray-600 text-sm">
+        &copy; 2024 EsmondYanintheGreg. All rights reserved.
+      </footer>
     </div>
+
   );
 }
