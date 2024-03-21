69d10f5708119a10642ed3599ff6b3e033cfe372
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-03-21 22:56:16 +0100

    [Update] Added a function to exclude request to websocket

--- backend/src/logger.js
+++ backend/src/logger.js
@@ -1,9 +1,20 @@
 const winston = require('winston');
 
+// Define a custom filter function to exclude WebSocket polling requests
+const excludeWebSocketPolling = winston.format((info, opts) => {
+  // Check if the message is a WebSocket polling request
+  if (info.message.includes('/ws/socket.io/?EIO=4&transport=polling')) {
+      // Exclude the message from being logged
+      return false;
+  }
+  return info;
+});
+
 // Initialize Winston logger
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.combine(
+      excludeWebSocketPolling(), // Apply custom filter function
       winston.format.timestamp({
         format: 'YYYY-MM-DD HH:mm:ss', // Customize timestamp format
       }),
