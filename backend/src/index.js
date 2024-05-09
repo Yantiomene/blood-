6ed0e0aa6b2851e0b607292c022804c4307087a1
Yantiomene <49885974+Yantiomene@users.noreply.github.com>
2024-05-09 09:23:25 +0100

    [Update] Adding new route

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,9 @@ const passport = require('passport');
 const cors = require('cors');
 const helmet = require('helmet');
 const logger = require('./logger');
+const { WebSocketServer } = require('ws');
+const { handleWebSocketMessages } = require('./controllers/websocketHandlers');
+const http = require('http');
 const app = express();
 
 
@@ -54,7 +57,26 @@ app.use((req, res) => {
     res.status(404).send('Route not found');
 });
 
-const appStart = () => {
+
+// Create HTTP server using Express app
+const server = http.createServer(app);
+
+// Initialize WebSocket server
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', (ws) => {
+    logger.info('WebSocket Client connected');
+
+    // Handle WebSocket messages
+    handleWebSocketMessages(ws);
+});
+
+// Start the server
+server.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+/*const appStart = () => {
     try {
         const server = app.listen(PORT, () => {
             console.log(`Server is running at http://localhost:${PORT}`);
@@ -66,4 +88,4 @@ const appStart = () => {
     }
 }
 
-appStart();
+appStart();*/
